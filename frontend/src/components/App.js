d6ee5a5183c6285639093fc46f7f9482a7ca6f28
denis <denis_13@hotmail.it>
2020-03-27 16:13:26 +0100

    Created layouts for Register and Login + routes (frontend)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,20 +1,23 @@
-import React, { Component, Fragment } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
-import { Provider as AlertProvider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
-import Header from './layout/Header';
-import Dashboard from './leads/Dashboard';
-import Alerts from './layout/Alerts';
+import Header from "./layout/Header";
+import Dashboard from "./leads/Dashboard";
+import Alerts from "./layout/Alerts";
+import Login from "./accounts/Login";
+import Register from "./accounts/Register";
 
-import { Provider } from 'react-redux';
-import store from '../store';
+import { Provider } from "react-redux";
+import store from "../store";
 
 // Alerts options
 const alertOptions = {
   timeout: 3000,
-  position: 'top center'
+  position: "top center"
 };
 
 class App extends Component {
@@ -22,17 +25,23 @@ class App extends Component {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Fragment>
-            <Header />
-            <Alerts />
-            <div className="container">
-              <Dashboard />
-            </div>
-          </Fragment>
+          <Router>
+            <Fragment>
+              <Header />
+              <Alerts />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Dashboard} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
         </AlertProvider>
       </Provider>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
