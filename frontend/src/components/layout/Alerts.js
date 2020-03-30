565a6b2ce46a3b496763fb7d64cb970da7004170
denis <denis_13@hotmail.it>
2020-03-30 11:53:46 +0200

    Alerts messages on Login/Registration page

--- frontend/src/components/layout/Alerts.js
+++ frontend/src/components/layout/Alerts.js
@@ -1,7 +1,7 @@
-import React, { Component, Fragment } from 'react';
-import { withAlert } from 'react-alert';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { Component, Fragment } from "react";
+import { withAlert } from "react-alert";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 export class Alerts extends Component {
   static propTypes = {
@@ -21,11 +21,18 @@ export class Alerts extends Component {
       if (error.msg.message) {
         alert.error(`Message: ${error.msg.message.join()}`);
       }
+      if (error.msg.non_field_errors) {
+        alert.error(error.msg.non_field_errors.join());
+      }
+      if (error.msg.username) {
+        alert.error(error.msg.username.join());
+      }
     }
 
     if (message !== prevProps.message) {
       if (message.deleteLead) alert.success(message.deleteLead);
       if (message.addLead) alert.success(message.addLead);
+      if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
     }
   }
 
